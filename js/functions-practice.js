6da51d7ee90209cca0cd6bddd8831e8a9518cfd9
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-05-31 13:16:37 -0500

    functions practice and exercises

---
+++ js/functions-practice.js
@@ -0,0 +1,13 @@
+function sayHello() {
+	console.log("Hello");
+}
+
+function sayBye(username) {
+	console.log("You will always see me");
+	 return "Peace out " + username;
+}
+
+let student = "Hector";
+function addToGreeting() {
+	return "Good morning," + student + "!";
+}
