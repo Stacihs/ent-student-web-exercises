29e104106f32a069428055348b41a886d8370c59
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-06-12 23:56:10 -0500

    Add while.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,22 +1,22 @@
 "use strict";
 
-// let userNumber = parseInt(prompt('Pick an odd number between 1 and 50'));
-// while (userNumber > 1 && userNumber < 50) {
-// 	// if (userNumber === isNaN()) {
-// 	// 	parseInt(prompt('Please enter a number'));
-// 	// 	break;
-// 	// }
+// TODO FIGURE OUT REPEAT PROMPT
+// let userNumber = parseInt(prompt('Give a number between 1 and 50'));
 //
-// 	if (userNumber) {
-// 		console.log(`Number to skip is: ${userNumber}`);
-// 		// continue;
+// while (userNumber >= 1 && userNumber <= 50) {
+//
+// 	console.log(`Number to skip is ${userNumber}`);
+// 	if (isNaN(userNumber)) {
+// 		parseInt(prompt('Give a number between 1 and 50'));
+// 		break;
 // 	}
 //
-// 	// for (let i = 1; i <= 50; i++) {
-// 	// 	if (userNumber % 2 !== 0) {
-// 	// 		console.log(`Here is an odd number: ${i}`);
-// 	// 	}
-// 	// }
+// 	for (let i = 1; i <= 50; i += 2) {
+// 		if (i === userNumber) {
+// 			console.log(`Yikes! Skipping number: ${userNumber}`);
+// 			continue;
+// 		}
+// 		console.log(`Here is an odd number: ${i}`);
+// 	}
+//
 // }
-
-
---
+++ js/while.js
@@ -0,0 +1,27 @@
+"use strict";
+// WHILE LOOP
+// let i = 2;
+// while ( i <= 65600 ) {
+// 	console.log(i);
+// 	i *= 2;
+// }
+
+
+// DO WHILE FIX THIS!!!!!!!!
+// const sellCones = () => {
+// 	let allCones = Math.floor(Math.random() * 50) + 50;
+// 	// let conesBought = Math.floor(Math.random() * 5) + 1;
+// 	let conesBought;
+// 	do {
+// 		conesBought = Math.floor(Math.random() * 5) + 1;
+// 		if (conesBought < allCones) {
+// 			console.log(`${conesBought} cones sold`);
+// 		} else if ( conesBought > allCones) {
+// 			console.log(`I can't sell you ${conesBought} cones. I only have ${allCones}`);
+// 		} else {
+// 			console.log(`Yay! I sold them all!`);
+// 		}
+// 	} while ( allCones > conesBought );
+// }
+
+
