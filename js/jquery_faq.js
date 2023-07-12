1e4c95330f59da0cfede8821ac6222b0b5d4c0f1
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-07-12 14:00:31 -0500

    jquery traversing exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -6,6 +6,26 @@
         $('dd').toggleClass('invisible');
     });
 
-    $('body').append(`<a href="#">Click Here</>`);
+    // $('body').append(`<a href="#">Click Here</>`);
+
+    $('button').on('click', function () {
+        $('ul').each(function (index, ulElement) {
+            $(ulElement).children()
+                .last().css('background-color', 'yellow')
+            }
+        )
+    })
 
 
+    $('h3').on('click',function () {
+        $('li').css('font-weight', 'bold')
+    })
+
+    $('li').on('click', function (event) {
+        $(event.target).parent().children().first().css('color', 'blue')
+            // .$('ul').parent().$('ul').children()
+            // .$('li').first().css('color', 'blue')
+    })
+
+// $('body').append('<button>Push Me</button>')
+//     .append('<h4>Practice</h4><div><ol><li></li><li></li></ol></div>')
