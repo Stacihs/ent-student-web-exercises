3ff1a674b01f5e2be855f47779d4ef0e0f54e7d8
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-07-14 12:15:15 -0500

    Add markers for top three restaurants mapbox_maps_api.js

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -1,13 +1,30 @@
 $(() => {
 
     // GLOBAL VARIABLES
-    const map = initializeCityFavRestaurantMap();
+    const map = initializeMap();
+    // const map = initializeCityFavRestaurantMap();
+    // const map = topThree();
     // const popup = createPopup();
     // const marker = createMarker();
+    const restaurants = [
+        {
+            "name": "Ida Claire",
+            "address": "7300 Jones Maltsberger Rd, San Antonio, TX 78209",
+            "phone": 210 - 667 - 2145
+        }, {
+            "name": "The Rustic",
+            "address": "7619 La Cantera Pkwy UNIT 204, San Antonio, TX 78257",
+            "phone": 210-245-7500
+        }, {
+            "name": "Jardin",
+            "address": "555 Funston Pl, San Antonio, TX 78209",
+            "phone": 210-338-5100
+        }
+    ]
 
     // FUNCTIONS
-    //     Create map with city of favorite restaurant
-    function initializeCityFavRestaurantMap() {
+    // Create a new map
+    function initializeMap() {
         mapboxgl.accessToken = MAPBOX_TOKEN;
 
         const mapOptions = {
@@ -16,48 +33,72 @@ $(() => {
             center: [-98.491142, 29.424349],
             zoom: 10
         }
-
-        geocode('136 E Grayson St Suite 120, San Antonio, TX 78215', MAPBOX_TOKEN).then((data) => {
-            console.log(data);
-            map.setCenter(data);
-            map.setZoom(15);
-            const southerleighPopup = new mapboxgl.Popup()
-                .setHTML(`<p>Southerleigh Fine Food And Brewery</p>`)
-            const restaurantMarker = new mapboxgl.Marker()
-                .setLngLat(data)
-                .addTo(map)
-                .setPopup(southerleighPopup);
-        })
-
         return new mapboxgl.Map(mapOptions);
     }
 
-    // restaurantMarker.setPopup(popup);
-    // const southerleighPopup = new mapboxgl.Popup()
-    //     .setHTML(`<p>Southerleigh Fine Food And Brewery</p>`);
-    //     .setPopup(southerleighPopup);
-    //      southerleighPopup.addTo(map);
 
-
-    // Create marker and set zoom for best viewing
-    // function createMarker() {
-    //     return
-    //         .setLngLat([-98.48104, 29.44258])
-    //         .addTo(map)
+    //     Create map with city of favorite restaurant
+    // function initializeCityFavRestaurantMap() {
+    //     mapboxgl.accessToken = MAPBOX_TOKEN;
+    //
+    //     const mapOptions = {
+    //         container: 'map',
+    //         style: 'mapbox://styles/mapbox/navigation-day-v1',
+    //         center: [-98.491142, 29.424349],
+    //         zoom: 10
+    //     }
+    //     geocode('136 E Grayson St Suite 120, San Antonio, TX 78215', MAPBOX_TOKEN).then((data) => {
+    //         console.log(data);
+    //         map.setCenter(data);
+    //         map.setZoom(15);
+    //         const southerleighPopup = new mapboxgl.Popup()
+    //             .setHTML(`<p>Southerleigh Fine Food And Brewery</p>`)
+    //         const restaurantMarker = new mapboxgl.Marker()
+    //             .setLngLat(data)
+    //             .addTo(map)
+    //             .setPopup(southerleighPopup);
+    //     })
+    //
+    //     return new mapboxgl.Map(mapOptions);
     // }
 
-    // Create popup with name of restaurant(after it has been clicked!)
-    // function createPopup() {
-    //     return new mapboxgl.Popup()
-    //         .setLngLat([-98.48104, 29.44258])
-    //         .setHTML(`
-    //             <div>
-    //                 <h1>Southerleigh Fine Food And Brewery</h1>
-    //             </div>
-    //         `);
-    // }
+    // REFACTOR Create markers for three favorite restaurants
+    function topThree() {
 
+        restaurants.forEach( (restaurant) => {
 
+            geocode(`${restaurant.address}`, MAPBOX_TOKEN).then((data) => {
+                console.log(data);
+                // map.setCenter(data);
+                // map.setZoom(15);
+                const restaurantPopup = new mapboxgl.Popup()
+                    .setHTML(`<h1>${restaurant.name}</h1><p>${restaurant.address}</p><p>${restaurant.phone}</p>`)
+                const restaurantMarker = new mapboxgl.Marker()
+                    .setLngLat(data)
+                    .addTo(map)
+                    .setPopup(restaurantPopup);
+            });
+        });
+    }
+
+topThree();
+// Create marker and set zoom for best viewing
+// function createMarker() {
+//     return
+//         .setLngLat([-98.48104, 29.44258])
+//         .addTo(map)
+// }
+
+// Create popup with name of restaurant(after it has been clicked!)
+// function createPopup() {
+//     return new mapboxgl.Popup()
+//         .setLngLat([-98.48104, 29.44258])
+//         .setHTML(`
+//             <div>
+//                 <h1>Southerleigh Fine Food And Brewery</h1>
+//             </div>
+//         `);
+// }
 
 
 });
