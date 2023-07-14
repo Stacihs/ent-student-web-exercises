1f31ffc2e607ee94479284bb19aab8b3ac35c8ff
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-07-13 20:43:43 -0500

    Add mapbox_maps_api files

---
+++ js/mapbox_maps_api.js
@@ -0,0 +1,40 @@
+$(() => {
+
+    // GLOBAL VARIABLES
+    const map = new mapboxgl.Map({
+       container: 'map',
+       style:
+    });
+
+    // FUNCTIONS
+    //     Create map with city of favorite restaurant
+
+
+
+    // Create marker and set zoom for best viewing
+    // Create popup with name of restaurant(after it has been clicked!)
+    // Add function to button to zoom
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
