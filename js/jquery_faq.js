e675281a6160f16d735ea2d4813fb32c13e2276f
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-07-15 19:09:03 -0500

    Add comments to jquery_faq.js and correct code on third event to bold font weight

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,24 +1,28 @@
 "use strict"
+$(() => {
 
-
-
+    // Toggles <dd> visibility
     $('a').on('click', function () {
         $('dd').toggleClass('invisible');
     });
 
+    // Highlights the last <li> child in a <ul>
     $('button').on('click', function () {
         $('ul').each(function (index, ulElement) {
-            $(ulElement).children()
-                .last().css('background-color', 'yellow')
+                $(ulElement).children()
+                    .last().css('background-color', 'yellow')
             }
         )
     })
 
-
-    $('h3').on('click',function () {
-        $('li').css('font-weight', 'bold')
+    //
+    $('h3').on('click',function (event) {
+        $(event.target).next().css('font-weight', 'bold')
     })
 
+    // Changes the color of the first <li> in the <ul> to blue
     $('li').on('click', function (event) {
         $(event.target).parent().children().first().css('color', 'blue')
     })
+});
+
