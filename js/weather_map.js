7e6c422caf1bc36f890f4c43a9fc12c797296a07
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-07-17 10:10:22 -0500

    Add search input to weather_map.html

--- js/weather_map.js
+++ js/weather_map.js
@@ -4,6 +4,7 @@ $(() => {
         // const OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
         // const map = initializeMap();
     const OPEN_WEATHER_URL_FIVE_DAY = "https://api.openweathermap.org/data/2.5/forecast";
+    // const map = initializeMap();
 
     /*****FUNCTIONS*****/
     // $.ajax(OPEN_WEATHER_URL, {
@@ -41,11 +42,10 @@ $(() => {
         renderFiveDayForecast(data);
     }).fail(console.error);
 
-
-        const renderFiveDayForecast = ((weatherStats) => {
-            weatherStats.list.forEach((day, index) => {
-                if (index % 8 === 0) {
-                    $('#weatherInfo').append(`<div class="weatherCard">
+    const renderFiveDayForecast = ((weatherStats) => {
+        weatherStats.list.forEach((day, index) => {
+            if (index % 8 === 0) {
+                $('#weatherInfo').append(`<div class="weatherCard">
                         <h2>${day.dt_txt}</h2>
                     <div><span id="temp">${parseInt(day.main.temp)}&deg;</span></div>
                      <h2>Feels Like</h2>
@@ -55,33 +55,54 @@ $(() => {
                     <h2>Wind</h2>
                     <div><span>${day.wind.speed} mph</span></div>
                     <h2>Conditions</h2>
-                    <div><span>${day.weather.description}</span></div>
+                    <div><span>${day.weather[0].description}</span></div>
                 </div>`);
-                }
-            });
+            }
         });
+    });
 
+    // function initializeMap() {
+    //     mapboxgl.accessToken = MAPBOX_TOKEN;
+    //
+    //     const mapOptions = {
+    //         container: 'map',
+    //         style: 'mapbox://styles/mapbox/navigation-day-v1',
+    //         center: [-98.491142, 29.424349],
+    //         zoom: 10
+    //     }
+    //     return new mapboxgl.Map(mapOptions);
+    // }
+    //
+    // function initializeCityFavRestaurantMap() {
+    //         mapboxgl.accessToken = MAPBOX_TOKEN;
+    //
+    //         const mapOptions = {
+    //             container: 'map',
+    //             style: 'mapbox://styles/mapbox/navigation-day-v1',
+    //             center: [-98.491142, 29.424349],
+    //             zoom: 10
+    //         }
+    //         geocode('136 E Grayson St Suite 120, San Antonio, TX 78215', MAPBOX_TOKEN).then((data) => {
+    //             console.log(data);
+    //             map.setCenter(data);
+    //             map.setZoom(15);
+    //             const southerleighPopup = new mapboxgl.Popup()
+    //                 .setHTML(`<p>Southerleigh Fine Food And Brewery</p>`)
+    //             const restaurantMarker = new mapboxgl.Marker()
+    //                 .setLngLat(data)
+    //                 .addTo(map)
+    //                 .setPopup(southerleighPopup);
+    //         })
+    //
+    //         return new mapboxgl.Map(mapOptions);
+    //     }
+    //
+    //     initializeCityFavRestaurantMap();
 
+    /*****EVENTS*****
+     //
 
-        //     Create map
-//     function initializeMap() {
-//         mapboxgl.accessToken = MAPBOX_TOKEN;
-//
-//         const mapOptions = {
-//             container: 'map',
-//             style: 'mapbox://styles/mapbox/navigation-day-v1',
-//             center: [-98.491142, 29.424349],
-//             zoom: 10
-//         }
-//         return new mapboxgl.Map(mapOptions);
-//     }
-//
-// initializeMap();
-
-        /*****EVENTS*****
-
-
-         /*****RUNS WHEN APP LOADS*****/
+     /*****RUNS WHEN APP LOADS*****/
 
 
 });
